3b3254ea43cb6e184440c3ef2fdeef080e92dea6
Nilun <dreamgodinu@gmail.com>
2021-10-02 11:18:49 +0200

    Ajout de la suppression par la page Admin Ajout de l'Edition par la page Admin Ajout de la consultation des stats par la page Admin Ajout de la Page Stats.php

--- Admin.php
+++ Admin.php
@@ -6,7 +6,7 @@
 	<body>
 	<?php include 'Navig.php';?>	
 
-<table class="Centered">
+
 <?php
  $serveurBD = "localhost";
 			$nomUtilisateur = "Admin";
@@ -19,15 +19,48 @@
 						  $baseDeDonnees);		  
 		  
 		  
+			if (isset ($_GET["Edit"]))
+				{
+					
+					$result = $mysqli->query("SELECT * FROM lien WHERE ID = ".$_GET["Edit"]) or die(mysqli_error($mysqli));	
+					$Value = $result->fetch_assoc();
+					echo "<Form Action='Admin.php'  class='centered' Method='POST'> ";
+					echo "<input type='TEXT' Value='".array_values($Value)[0]."' Name='IDEdit' readonly='readonly' /> <br>";
+					echo "Lien Long : <input type='TEXT' Value= '".array_values($Value)[1]."' Name = 'EditNewLong'/> <br/> ";				
+					echo "Lien Court  : <input type='TEXT' Value= '".array_values($Value)[2]."' Name = 'EditNewCourt'/> <br/> ";				
+					echo "<input type = 'submit' value ='Valid'>";
+					echo "</Form><Br><Br><Br><Br><HR Width = '25%'>";
+					
+				}				
+				
+			if (isset ($_POST["EditNewLong"]) && isset ($_POST["EditNewCourt"]) && isset($_POST["IDEdit"]))
+				{
+					
+					$result = $mysqli->query("UPDATE lien
+											  SET Lien_Long = '".$_POST["EditNewLong"]."' , Lien_Court='".$_POST["EditNewCourt"]."'
+											  WHERE ID = ".$_POST["IDEdit"]) or die(mysqli_error($mysqli));	
+					
+					
+				}
+			if (isset ($_GET["DELETE"]))
+				{
+					$result = $mysqli->query("DELETE FROM lien	WHERE ID = ".$_GET["DELETE"]) or die(mysqli_error($mysqli));	
+					$result = $mysqli->query("DELETE FROM stat	WHERE ID = ".$_GET["DELETE"]) or die(mysqli_error($mysqli));
+				}
 		    $result = $mysqli->query("SELECT * FROM lien ") or die(mysqli_error($mysqli));			
+			
+			echo "<table class='Centered'>"	;
+			
 			
 			 while ($row = $result -> fetch_row())
 				 {				 
-					echo ("<tr><td>". $row[0]."</td><td>" .$row[1]."</td><td>".$_SERVER['SERVER_NAME'] ."/r".$row[2]."</td><td><a href='Delete.php?T=".$row[0]."'><img src='cross.png'/></a></td><td><a href='Edit.php?T=".$row[0]."'><img src='edit.png'/></a></td><td><a href='Stats.php?T=".$row[0]."'><img src='stats.png'/></a></td></tr>");			
+					echo ("<tr><td>". $row[0]."</td><td>" .$row[1]."</td><td>".$_SERVER['SERVER_NAME'] ."/r".$row[2]."</td><td><a href='Admin.php?DELETE=".$row[0]."'><img src='cross.png'/></a></td><td><a href='Admin.php?Edit=".$row[0]."'><img src='edit.png'/></a></td><td><a href='Stats.php?T=".$row[0]."'><img src='stats.png'/></a></td></tr>");			
 				}
 				
+				
+			echo "</table>";
 			$result -> free_result();
 ?>
-</table>
+
 </body>
 </html>
---
+++ Stats.php
@@ -0,0 +1,51 @@
+<html>
+	<header>			
+		<link rel="stylesheet" href="main.css">
+	</header>
+	
+	<body>
+		<?PHP
+		 include 'Navig.php';
+		  if (isset ($_GET["T"]))
+		  {
+			  $serveurBD = "localhost";
+					$nomUtilisateur = "Admin";
+					$motDePasse = "c344RqtvsgGCA8K";
+					$baseDeDonnees = "shortener";
+				   
+					$mysqli = new mysqli($serveurBD,
+								  $nomUtilisateur,
+								  $motDePasse,
+								  $baseDeDonnees);		
+			$result = $mysqli->query("SELECT * FROM lien Where ID =".$_GET["T"]) or die(mysqli_error($mysqli));			
+					
+			echo "<table class='Centered'>"	;
+					
+					
+			$Value = $result->fetch_assoc();					
+			$DateCrea = array_values($Value)[3]		;	
+			echo "Lien Long :  <a href ='".array_values($Value)[1]."' >".array_values($Value)[1]."  </a> <br/> ";				
+			echo "Lien Court  : ".$_SERVER['SERVER_NAME'] ."/r".array_values($Value)[2]."  <br/> ";
+			echo "Date de Creation :".$DateCrea ."<br>"; 
+			
+			$result = $mysqli->query("SELECT Count(*) FROM stat Where ID =".$_GET["T"]) or die(mysqli_error($mysqli));							
+			$Value = $result->fetch_assoc();					
+			$NbClick = array_values($Value)[0];
+			
+			$result = $mysqli->query("Select MAX(TT) From (SELECT Count(*) AS TT , date FROM stat Where ID =".$_GET["T"]." GROUP BY CAST(Date AS DATE) )AS SUBQUERY  ") or die(mysqli_error($mysqli));							
+			$Value = $result->fetch_assoc();					
+			$MaxJour = array_values($Value)[0];
+			
+			$timeDiff = abs(time() - strtotime($DateCrea));
+			$numberDays = $timeDiff/86400; // nb second 
+			$numberDays = intval($numberDays);
+			Echo "Créer depuis : " . $numberDays . " Jours <br/>";
+			echo "Nombre Consultation :  ".$NbClick."  <br/> ";	
+			echo "Nombre de Consultation Max par Jour : ". $MaxJour . "<br>";		
+			if($numberDays>0) {	echo "Nombre de Consultation Moyenne par Jour : "  . ($NbClick /$numberDays) ;}
+		
+						
+		  }
+		?>
+	</body>
+</HTML>

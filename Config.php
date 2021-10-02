c8bf938a5157c24d3ad456affdac52323590487b
Nilun <dreamgodinu@gmail.com>
2021-10-03 00:13:09 +0200

    Documentation

--- Config.php
+++ Config.php
@@ -2,14 +2,14 @@
 function GetConfig()
 {
 	$Config = 
-	array( "User" => "test",
-      "Password" => "test",
-	  "MailUser" => "Your mail",
-      "MailPassword" => "",
-	  "MailFrom" => "sender@exemple.com",
-      "MailTo" => "your mail box@exemple.com",
-	  "MailPort"=> 465,
-	  "MailHost"=> "smtp.gmail.com",
+	array( "User" => "test", // Default Admin User
+      "Password" => "test", // Default Admin Password
+	  "MailUser" => "Your mail", // SMTP Mail User
+      "MailPassword" => "", // SMTP Mail Password
+	  "MailFrom" => "sender@exemple.com", // Sender Email Adress
+      "MailTo" => "your mail box@exemple.com",// The Email Adress you want to receive the mail
+	  "MailPort"=> 465,// SMTP server port
+	  "MailHost"=> "smtp.gmail.com",// SMTP server
 	  
 	);
 	return $Config;
